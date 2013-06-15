b99ae5034f2d358ee81464a49dd6723a7fac656d
zanardi <f.abeni@gibilogic.com>
2013-06-15 12:12:21 +0200

    Change FOSUserBundle version

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Userstories\CoreBundle\UserstoriesCoreBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Userstories\CoreBundle\UserstoriesCoreBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Userstories/CoreBundle/Entity/User.php
+++ src/Userstories/CoreBundle/Entity/User.php
@@ -8,20 +8,22 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * User
  *
- * @ORM\Table()
- * @ORM\Entity()
+ * @ORM\Entity
+ * @ORM\Table(name="user")
  */
 class User extends BaseUser
 {
     /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     /**
      * Get id
